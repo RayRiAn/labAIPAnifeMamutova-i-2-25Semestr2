6d626f7998886858f3654ff938fb61c3a15711d3
RayRiAn <sunanxt24@gmail.com>
2026-03-13 14:42:03 +0300

    Add files via upload

---
+++ laba4Var1ZadanieC.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int number;
+    cout << "Введите число: ";
+    cin >> number;
+
+    int* ptr = &number;
+
+    cout << "\nЗначение числа (через указатель): " << *ptr << endl;
+    cout << "Адрес числа в памяти: " << ptr << endl;
+
+    (*ptr)++;
+
+    cout << "\nПосле увеличения на 1:" << endl;
+    cout << "Новое значение числа: " << number << endl;
+    cout << "Значение через указатель: " << *ptr << endl;
+
+    return 0;
+}
