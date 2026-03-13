978b756cc06dd820aaaf1c1e7f139a1e40375333
RayRiAn <sunanxt24@gmail.com>
2026-03-13 14:52:28 +0300

    Add files via upload

---
+++ lab4Var1ZadanieD.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    char symbol;
+    char* ptr = &symbol;
+
+    cout << "Введите любой символ: ";
+    cin >> symbol;
+
+    cout << "\nВведенный символ: " << symbol << endl;
+    cout << "Символ через указатель: " << *ptr << endl;
+    cout << "Адрес символа: " << (void*)ptr << endl; 
+
+    (*ptr)++; 
+
+    cout << "\nСледующий символ в алфавите: " << symbol << endl;
+    cout << "Новый символ через указатель: " << *ptr << endl;
+
+    return 0;
+}
