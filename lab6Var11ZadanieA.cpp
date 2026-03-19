7a8d9b5f6fb1ce806ad146dd26ff0182f0e6bdba
RayRiAn <sunanxt24@gmail.com>
2026-03-19 16:59:08 +0300

    Add files via upload

---
+++ lab6Var11ZadanieA.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    string words[] = { "C++", "is", "one", "of", "the", "world's", "most", "popular", "programming", "languages" };
+
+    int size = 10;
+    string** ptrArray = new string * [size];
+
+    for (int i = 0; i < size; i++) {
+        ptrArray[i] = &words[i];
+    }
+
+    cout << "Исходный порядок:" << endl;
+    for (int i = 0; i < size; i++) {
+        cout << *ptrArray[i] << " ";
+    }
+    cout << endl;
+
+    int polovina = size / 2;
+
+    for (int i = 0; i < polovina; i++) {
+        string* itog = ptrArray[i];
+        ptrArray[i] = ptrArray[i + polovina];
+        ptrArray[i + polovina] = itog;
+    }
+
+    cout << "После замены половин:" << endl;
+    for (int i = 0; i < size; i++) {
+        cout << *ptrArray[i] << " "; 
+    }
+    cout << endl;
+
+
+    delete[] ptrArray;
+
+    return 0;
+}
